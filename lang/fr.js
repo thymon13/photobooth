af9b4c26888451f8625a6c9f0f983dbd7c50770e
Andreas Blaesius <andi@unlegacy-android.org>
2019-08-01 17:12:41 +0200

    enable/disable CUPS button via config/admin panel

--- lang/fr.js
+++ lang/fr.js
@@ -35,6 +35,7 @@ L10N = {
 	"file_format_date": "Utilisez images au format date",
 	"chroma_keying": "Autoriser l'incrustation chromatique",
 	"keyingerror" : "L'incrustation chroma n'est pas possible!",
+	"cups_button": "Afficher le bouton CUPS",
 	"use_print": "Utiliser l'impression",
 	"use_qr": "Utiliser les codes QR",
 	"print_qrcode" : "QR-Code sur l'image en cours d'impression",
