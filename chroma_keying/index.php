390f076cb068c0c0ccf2b127ff663ce8aa07ca30
Andreas Blaesius <andi@unlegacy-android.org>
2019-08-02 09:58:15 +0200

    chroma keying: translate && properly style buttons

--- chroma_keying/index.php
+++ chroma_keying/index.php
@@ -1,10 +1,20 @@
 <?php
+$my_config = '../my.config.inc.php';
+if (file_exists($my_config)) {
+	require_once('../my.config.inc.php');
+} else {
+	require_once('../config.inc.php');
+}
+
 $location = base64_decode($_GET['location']);
 $filename = $_GET['filename'];
 ?>
 <!doctype html>
 <html>
 <head>
+<link rel="stylesheet" href="/resources/css/normalize.css" />
+<link rel="stylesheet" href="/resources/css/default-skin/default-skin.css">
+<link rel="stylesheet" href="/resources/css/style.css" />
 <script type="text/javascript" src="lib/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="lib/marvinj-0.8.js"></script>
 <script type="text/javascript" src="lib/custom.js"></script>
@@ -39,11 +49,12 @@ function navigateToMain() {
 	</div>
 
 	<div style="padding-top:10px;text-align:center;">
-		<button style="width:150px;height:50px;font-size:20px;cursor:pointer;" onclick="saveImage()">Speichern</button>
-		<button style="width:150px;height:50px;font-size:20px;cursor:pointer;" onclick="printImage()">Drucken</button>
-		<button style="width:150px;height:50px;font-size:20px;cursor:pointer;" onclick="navigateToMain()">Schlie&szlig;en</button>
+		<button class="btn" style="width:150px;height:50px;font-size:20px;cursor:pointer;" onclick="saveImage()"><span data-l10n="save"></span></button>
+		<button class="btn" style="width:150px;height:50px;font-size:20px;cursor:pointer;" onclick="printImage()"><span data-l10n="print"></span></button>
+		<button class="btn" style="width:150px;height:50px;font-size:20px;cursor:pointer;" onclick="navigateToMain()"><span data-l10n="close"></span></button>
 	</div>
 </div>
-
+	<script type="text/javascript" src="/resources/js/l10n.js"></script>
+	<script type="text/javascript" src="/lang/<?php echo $config['language']; ?>.js"></script>
 </body>
 </html>
