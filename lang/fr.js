68514bef2407a2ba20fbee444d8dff6d1f26d373
Thymon <jeanmichel.amphoux@gmail.com>
2019-09-17 23:47:08 +0200

    Update fr.js
    Sorry mistakes...

--- lang/fr.js
+++ lang/fr.js
@@ -20,7 +20,7 @@ L10N = {
 	"error": "Il y a eu un problème. Merci de réessayer",
 	"reload": "Recharger la page",
 	"print": "Imprimer",
-	"printing": "I'impression à commencé",
+	"printing": "L'impression a commencé",
 	"save":"Sauvegarder",
 	"saving":"En cours de sauvegarde",
 	"success":"Enregistré avec succès",
